545c197186c9508be41281e968db41caf00be06e
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-01 16:07:30 -0300

    Create menu.php

---
+++ docs/menu.php
@@ -0,0 +1,53 @@
+<?php
+	include_once 'php/class.usuario.php';
+	include_once 'php/class.usuario.control.php';
+	include_once 'php/conexion.inc.php';
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap">
+
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+		<div class="menu">
+            <!--img class="logo" src="image/vyv_logo_3.png" alt="logo vyv s.a."-->
+
+            <lu class="items">
+                <li class="item-menu">
+                    <h2 class="active">Pefiles</h2>
+                </li>
+
+                <li class="item-menu">
+                    <h2>Usuarios</h2>
+                </li>
+
+                <li class="item-menu">
+                    <h2>Productos</h2>
+                </li>
+
+                <li class="item-menu">
+                    <h2>Compras</h2>
+                </li>
+
+                <li class="item-menu">
+                    <h2>Ventas</h2>
+                </li>
+            </lu>
+        </div>
+
+		<!-- Javascript de boostrap -->
+ 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	</body>
+</html>
