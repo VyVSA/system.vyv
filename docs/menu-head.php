628b6830fadcee648d20c322489720ee13069c20
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-02 01:40:26 -0300

    Update menu-head.php

--- docs/menu-head.php
+++ docs/menu-head.php
@@ -1,30 +1,22 @@
 
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/estilo.css">
-	</head>
+<div class="menu">
 
-	<body>
-        <div class="menu">
+    <img class="logo" src="image/vyv_logo_3.png" alt="logo vyv s.a.">
 
-            <img class="logo" src="image/vyv_logo_3.png" alt="logo vyv s.a.">
-
-            <lu class="items">
-                <li class="item-menu" id="item-perfiles"><h2 class="">Pefiles</h2></li>
-                <li class="item-menu" id="item-usuarios"><h2 class="">Usuarios</h2></li>
-                <li class="item-menu" id="item-productos"><h2 class="">Productos</h2></li>
-                <li class="item-menu" id="item-compras"><h2 class="">Compras</h2></li>
-                <li class="item-menu" id="item-ventas"><h2 class="">Ventas</h2></li>
-            </lu>
+    <lu class="items">
+        <li class="item-menu" id="item-perfiles"><h2 class="">Pefiles</h2></li>
+        <li class="item-menu" id="item-usuarios"><h2 class="">Usuarios</h2></li>
+        <li class="item-menu" id="item-productos"><h2 class="">Productos</h2></li>
+        <li class="item-menu" id="item-compras"><h2 class="">Compras</h2></li>
+        <li class="item-menu" id="item-ventas"><h2 class="">Ventas</h2></li>
+    </lu>
             
-            <button class="btn-cuenta">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                </svg>
-                Cuenta
-            </button>
+    <button class="btn-cuenta">
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+        </svg>
+        Cuenta
+    </button>
 
-        </div>
-    </body>
-</html>
+</div>
