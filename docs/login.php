feedda8acfccdcb661b6a0a4ed07b8e97ca02af7
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-02 20:43:27 -0300

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -9,7 +9,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<link rel="stylesheet" type="text/css" href="css/login.css">
 
 		<link rel="preconnect" href="https://fonts.gstatic.com">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap">
