7de4ff582647a28c95313791ac899ffe276b2591
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-04 00:17:46 -0300

    Update registro-perfiles.php

--- docs/registro-perfiles.php
+++ docs/registro-perfiles.php
@@ -32,7 +32,7 @@
 
 					<label class="font-18px label-name">Nombre</label>
 					<input type="text" name="nombre" id="input-nombre" class="form-control">
-					
+
 					<div class="functions-perfiles">
 						<label class="check-text">
 
@@ -85,7 +85,7 @@
 						<label class="check-text label-margin-top">
 
 							<!-- input check bajar-perfil -->
-							<input type="checkbox" class="bajar-perfil" value="4">
+							<input type="checkbox" name="bajar-perfil" value="4">
 							<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-eye checked" viewBox="0 0 16 16">
 								<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
 								<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
@@ -151,7 +151,7 @@
 						<label class="check-text label-margin-top">
 
 							<!-- input check bajar-compra -->
-							<input type="checkbox" name="bajar-compra" valeu="16">
+							<input type="checkbox" name="bajar-compra" value="16">
 							<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-eye checked" viewBox="0 0 16 16">
 								<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
 								<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
@@ -264,10 +264,10 @@
 						</label>
 						<label class="font-16px label-function">Registro de pedidos</label>
 
-						<label class="check-text label-margin-top" value="19">
+						<label class="check-text label-margin-top">
 
 							<!-- input check mantener-pedido -->
-							<input type="checkbox" name="mantener-pedido">
+							<input type="checkbox" name="mantener-pedido" value="19">
 							<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-eye checked" viewBox="0 0 16 16">
 								<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
 								<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
@@ -362,7 +362,7 @@
 						</label>
 						<label class="font-16px label-function">Baja de productos</label>
 					</div>
-					
+
 					<div class="functions-pedidos"></div>
 
 					<!-- input submit iniciar sesión -->
@@ -405,6 +405,9 @@
 							if (isset($_POST['mantener-pedido'])) {array_push($funciones, $_POST['mantener-pedido']);}
 							if (isset($_POST['bajar-pedido'])) {array_push($funciones, $_POST['bajar-pedido']);}
 
+							$perfil = new Perfil_Boundary($nombre, $funciones);
+							$perfil->registrar($nombre, $funciones);
+							
 							Conexion::abrir_conexion();
 
 							Conexion::cerrar_conexion();
