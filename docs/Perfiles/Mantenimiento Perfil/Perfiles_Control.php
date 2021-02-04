fdf6c85ccc46b0c11b0d41330ee7d0eeed2d14d9
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-03 22:25:35 -0300

    Create Perfiles_Control.php

---
+++ docs/Perfiles/Mantenimiento Perfil/Perfiles_Control.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mantenimiento Perfil;
+
+class Perfiles_Control
+{
+
+
+
+
+	public function __construct()
+	{
+		// ...
+	}
+
+	public function listar_perfiles(): array
+	{
+		// TODO implement here
+		return [];
+	}
+
+	public function eliminar_perfil(Integer $id): boolean
+	{
+		// TODO implement here
+		return false;
+	}
+
+	public function actualizar(Integer $id, Integer $funciones): void
+	{
+		// TODO implement here
+		return null;
+	}
+
+}
