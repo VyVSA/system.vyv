7ef31a6fa7085c39fe76aa8901c5bbb38636f450
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-03 22:20:16 -0300

    Update conexion.inc.php

--- docs/php/conexion.inc.php
+++ docs/php/conexion.inc.php
@@ -13,12 +13,9 @@
 					self::$conexion = new PDO("mysql:host=$nombre_servidor; dbname=$nombre_base_datos", $nombre_usuario, $contrasenha);
 					self::$conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 					self::$conexion -> exec("SET CHARACTER SET utf8");
-
 				} catch (PDOException $e) {
-					
 					print "ERROR: ".$e -> getMessage()."<br>";
 					die();
-
 				}
 
 			}
@@ -28,17 +25,13 @@
 		public static function cerrar_conexion()	{
 
 			if (isset(self::$conexion))	{
-
 				self::$conexion = null;
-
 			}
 
 		}
 
 		public static function obtener_conexion()	{
-
 			return self::$conexion;
-
 		}
 		
 	}
