99e5cac61a07d239f6b37be0f7c3c516ffaebb1c
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-03 22:25:25 -0300

    Create Lista_Perfiles.php

---
+++ docs/Perfiles/Crear Perfil/Lista_Perfiles.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crear Perfil;
+
+class Lista_Perfiles
+{
+
+	private array $perfiles;
+
+
+
+
+
+	public function __construct()
+	{
+		// ...
+	}
+
+	public function verificar(Integer $funciones): boolean
+	{
+		// TODO implement here
+		return false;
+	}
+
+	public function get_perfiles(): array
+	{
+		// TODO implement here
+		return [];
+	}
+
+	public function agregar([object Object] $perfil): void
+	{
+		// TODO implement here
+		return null;
+	}
+
+	public function eliminar(Integer $id): boolean
+	{
+		// TODO implement here
+		return false;
+	}
+
+	public function buscar(Integer $id): [object Object]
+	{
+		// TODO implement here
+		return null;
+	}
+
+}
