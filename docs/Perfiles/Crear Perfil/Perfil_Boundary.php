7ac036c843be96ef4de372926b7ef4867c7ef072
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-03 22:25:27 -0300

    Create Perfil_Boundary.php

---
+++ docs/Perfiles/Crear Perfil/Perfil_Boundary.php
@@ -0,0 +1,19 @@
+<?php
+
+class Perfil_Boundary{
+
+	private $nombre;
+	private $funciones = array(); /* array */
+
+	public function __construct($nombre, $funciones){
+		$this->nombre = $nombre;
+		$this->funciones = $funciones;
+	}
+
+	public function registrar($nombre, $funciones){
+		
+	}
+
+}
+
+?>
