0d0328ed4b2d596f66779ccdb4beef083462755b
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-03 22:25:37 -0300

    Create Perfiles_Boundary.php

---
+++ docs/Perfiles/Mantenimiento Perfil/Perfiles_Boundary.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mantenimiento Perfil;
+
+class Perfiles_Boundary
+{
+
+
+	public function __construct()
+	{
+		// ...
+	}
+
+	public function listar_perfiles(): array
+	{
+		// TODO implement here
+		return [];
+	}
+
+	public function eliminar_perfil(Integer $id): boolean
+	{
+		// TODO implement here
+		return false;
+	}
+
+	public function actualizar(Integer $id, Integer $funciones): boolean
+	{
+		// TODO implement here
+		return false;
+	}
+
+}
