7688a8c41f5ec0fa6b8d0e9670beae30fd56943e
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-03 22:25:29 -0300

    Create Perfil_Control.php

---
+++ docs/Perfiles/Crear Perfil/Perfil_Control.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crear Perfil;
+
+class Perfil_Control
+{
+
+
+
+
+	public function __construct()
+	{
+		// ...
+	}
+
+	public function registrar(String $nombre, Integer $funciones, Integer $tipo): boolean
+	{
+		// TODO implement here
+		return false;
+	}
+
+}
