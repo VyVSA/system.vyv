a1b9115f6544705c9f12f71b6342c6c4b6372309
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-08 16:01:36 -0300

    Update Usuario_Control.php

--- docs/php/usuarios/crear-usuario/Usuario_Control.php
+++ docs/php/usuarios/crear-usuario/Usuario_Control.php
@@ -9,6 +9,28 @@
 		}
 
 		public function registrar($documento, $nombres, $apellidos, $celular, $email, $direccion, $perfil){
+			
+			/* registro del nuevo perfil */
+			try {
+				$querySql="	INSERT INTO
+								usuario
+							VALUES(
+								'',
+								'$documento',
+								'$nombres',
+								'$apellidos',
+								'$celular',
+								'$email',
+								'$direccion',
+								'$perfil'
+							)";
+
+				$sentencia = $conexion->prepare($querySql);
+				$sentencia->execute();
+			} catch (Exception $e) {
+				print "ERROR: ".$e->getMessage();
+			}
+
 			return null;
 		}
 
