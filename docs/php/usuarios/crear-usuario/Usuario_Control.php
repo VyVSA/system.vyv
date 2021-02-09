8c183fabc039c9c688f3cd8e94af9909805ff57b
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-08 21:39:18 -0300

    modify

--- docs/php/usuarios/crear-usuario/Usuario_Control.php
+++ docs/php/usuarios/crear-usuario/Usuario_Control.php
@@ -1,49 +1,25 @@
 <?php
-
 	class Usuario_Control{
-
 		public function __construct(){}
 
-		public function verificar($documento){
-			return false;
-		}
+		public function verificar($documento){return false;}
 
 		public function registrar($conexion, $documento, $nombres, $apellidos, $celular, $email, $direccion, $perfil){
-			
-			/* registro del nuevo perfil */
 			try {
 				$querySql="	INSERT INTO
 								usuario(
-									documento,
-									nombre,
-									apellido,
-									celular,
-									email,
-									direccion,
-									perfil
+									documento, nombre, apellido, celular, email, direccion, perfil
 								)
 							VALUES(
-								'$documento',
-								'$nombres',
-								'$apellidos',
-								'$celular',
-								'$email',
-								'$direccion',
-								'$perfil'
+								'$documento', '$nombres', '$apellidos', '$celular', '$email', '$direccion', '$perfil'
 							)";
 				$sentencia = $conexion->prepare($querySql);
 				$sentencia->execute();
 			} catch (Exception $e) {
 				print "ERROR: ".$e->getMessage();
 			}
-
-			return null;
 		}
 
-		public function actualizar($id, $datos){
-			return false;
-		}
-
+		public function actualizar($id, $datos){return false;}
 	}
-
 ?>
