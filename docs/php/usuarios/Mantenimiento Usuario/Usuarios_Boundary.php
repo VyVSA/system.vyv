4646ee3078bffd322dccd8207d0d169785c24796
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-10 10:54:47 -0300

    Update Usuarios_Boundary.php

--- docs/php/usuarios/Mantenimiento Usuario/Usuarios_Boundary.php
+++ docs/php/usuarios/Mantenimiento Usuario/Usuarios_Boundary.php
@@ -1,39 +1,26 @@
 <?php
 
-declare(strict_types=1);
-
-namespace Mantenimiento Usuario;
-
 class Usuarios_Boundary
 {
-
 
 	public function __construct()
 	{
 		// ...
 	}
 
-	public function listar_usuarios(): array
-	{
-		// TODO implement here
-		return [];
-	}
-
-	public function baja(Integer $id): boolean
-	{
-		// TODO implement here
-		return false;
-	}
-
-	public function set_contraseña(Integer $id, String $contraseña): void
-	{
-		// TODO implement here
+	public function listar_usuarios(): array{
 		return null;
 	}
 
-	public function set_perfil(Integer $id_usuario, Integer $id_perfil): void
-	{
-		// TODO implement here
+	public function baja($id){
+		return false;
+	}
+
+	public function set_contraseña($id, String $contraseña){
+		return null;
+	}
+
+	public function set_perfil($id_usuario, $id_perfil){
 		return null;
 	}
 
