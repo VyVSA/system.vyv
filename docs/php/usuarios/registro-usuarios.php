57856166257aa7aad74da494b36bb9c91cf0bf3b
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-12 01:37:59 -0300

    Update registro-usuarios.php

--- docs/php/usuarios/registro-usuarios.php
+++ docs/php/usuarios/registro-usuarios.php
@@ -68,10 +68,10 @@
 								<td class="td-perfil"><?php echo $usuario[8] ?></td>
 								<td class="td-button">
 									
-										<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-											<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-											<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-										</svg>
+									<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+										<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+										<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+									</svg>
 
 								</td>
 								<td class="td-button">
@@ -87,106 +87,89 @@
 			<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
 				<div class="modal-dialog">
 					<div class="modal-content div-registro-usuario">
-							<!-- formulario de registro de perfiles -->
-							<form method="POST" name="form-usuario">
-								<label class="font-24px label-title">Registro de usuario</label>
+						<!-- formulario de registro de perfiles -->
+						<form method="POST" name="form-usuario">
+							<label class="font-24px label-title">Registro de usuario</label>
 
-								<!-- input documento -->
-								<label class="font-18px label-document">Documento</label>
-								<input type="text" name="documento" class="form-control width-100">
+							<!-- input documento -->
+							<label class="font-18px label-document">Documento</label>
+							<input type="text" name="documento" class="form-control width-100">
 
-								<!-- input nombres -->
-								<label class="font-18px label">Nombres</label>
-								<input type="text" name="nombres" class="form-control width-100">
+							<!-- input nombres -->
+							<label class="font-18px label">Nombres</label>
+							<input type="text" name="nombres" class="form-control width-100">
 
-								<!-- input apellidos -->
-								<label class="font-18px label">Apellidos</label>
-								<input type="text" name="apellidos" class="form-control width-100">
+							<!-- input apellidos -->
+							<label class="font-18px label">Apellidos</label>
+							<input type="text" name="apellidos" class="form-control width-100">
 
-								<!-- input celular -->
-								<label class="font-18px label">Celular</label>
-								<input type="text" name="celular" class="form-control width-100">
+							<!-- input celular -->
+							<label class="font-18px label">Celular</label>
+							<input type="text" name="celular" class="form-control width-100">
 
-								<!-- input email -->
-								<label class="font-18px label">Email</label>
-								<input type="text" name="email" class="form-control width-100">
+							<!-- input email -->
+							<label class="font-18px label">Email</label>
+							<input type="text" name="email" class="form-control width-100">
 
-								<!-- input dirección -->
-								<label class="font-18px label">Dirección</label>
-								<input type="text" name="direccion" class="form-control width-100">
+							<!-- input dirección -->
+							<label class="font-18px label">Dirección</label>
+							<input type="text" name="direccion" class="form-control width-100">
 
-								<!-- Select perfil -->
-								<label class="font-18px label">perfil</label>
-								<div class="input-group divv">
-									<?php
-										$querySql = "SELECT * FROM perfil;";
-										Conexion::abrir_conexion();
-										$conexion = Conexion::obtener_conexion();
-										$sentencia = $conexion->prepare($querySql);
-										$sentencia->execute();
-										$perfiles = $sentencia->fetchAll();
-										Conexion::cerrar_conexion();
-									?>
-									<select name="perfil" id="input-perfil" class="form-control input-group">
-										<option></option>
-										<?php foreach ($perfiles as $perfil) {?><option value="<?php echo $perfil[0]?>"><?php echo $perfil[1]?></option><?php }?>
-									</select>
-									<div>
-										<button type="button" id="button-funciones" class="btn form-control" onclick="">Funciones</button>
-									</div>
+							<!-- Select perfil -->
+							<label class="font-18px label">perfil</label>
+							<div class="input-group divv">
+								<?php
+									$querySql = "SELECT * FROM perfil;";
+									Conexion::abrir_conexion();
+									$conexion = Conexion::obtener_conexion();
+									$sentencia = $conexion->prepare($querySql);
+									$sentencia->execute();
+									$perfiles = $sentencia->fetchAll();
+									Conexion::cerrar_conexion();
+								?>
+								<select name="perfil" id="input-perfil" class="form-control input-group">
+									<option></option>
+									<?php foreach ($perfiles as $perfil) {?><option value="<?php echo $perfil[0]?>"><?php echo $perfil[1]?></option><?php }?>
+								</select>
+								<div>
+									<button type="button" id="button-funciones" class="btn form-control" onclick="">Funciones</button>
 								</div>
+							</div>
 
-								<!-- inputs submit -->
-								<input type="submit" name="cancelar" id="input-cancelar" class="btn" value="Cancelar">
-								<input type="submit" name="registrar" id="input-registrar" class="btn" value="Registrar">
-							</form>
-							<!-- conexión a la base de datos y consulta a la misma para comprobar las credenciales -->
-							<?php
-								if (isset($_POST['registrar'])) { $documento = $_POST['documento'];
-									if ($documento != '' && is_numeric($documento)) { $nombres = trim($_POST['nombres']);
-										if ($nombres != '' && is_string($nombres)) { $apellidos = trim($_POST['apellidos']);
-											if ($apellidos != '' && is_string($apellidos)) { $email = str_replace(' ', '', trim($_POST['email']));
-												if ($email != '' && is_string($email)) { $perfil = $_POST['perfil'];
-													if ($perfil != '' && is_numeric($perfil)) {
-														$celular = $_POST['celular'];
-														$direccion = $_POST['direccion'];
+							<!-- inputs submit -->
+							<input type="submit" name="cancelar" id="input-cancelar" class="btn" value="Cancelar">
+							<input type="submit" name="registrar" id="input-registrar" class="btn" value="Registrar">
+						</form>
+						<!-- conexión a la base de datos y consulta a la misma para comprobar las credenciales -->
+						<?php
+							if (isset($_POST['registrar'])) { $documento = $_POST['documento'];
+								if ($documento != '' && is_numeric($documento)) { $nombres = trim($_POST['nombres']);
+									if ($nombres != '' && is_string($nombres)) { $apellidos = trim($_POST['apellidos']);
+										if ($apellidos != '' && is_string($apellidos)) { $email = str_replace(' ', '', trim($_POST['email']));
+											if ($email != '' && is_string($email)) { $perfil = $_POST['perfil'];
+												if ($perfil != '' && is_numeric($perfil)) {
+													$celular = $_POST['celular'];
+													$direccion = $_POST['direccion'];
 
-														Conexion::abrir_conexion();
-														$conexion = Conexion::obtener_conexion();
-														$usuario = new Usuario_Boundary($conexion, $documento, $nombres, $apellidos, $celular, $email, $direccion, $perfil);
+													Conexion::abrir_conexion();
+													$conexion = Conexion::obtener_conexion();
+													$usuario = new Usuario_Boundary($conexion, $documento, $nombres, $apellidos, $celular, $email, $direccion, $perfil);
 
-														$var = $usuario->registrar();
-														if ($var) {
-															echo	'<script type="text/javascript">
-																		alert("Se registró");
-																	</script>';
-														}else {
-															echo	'<script type="text/javascript">
-																		alert("No se registró naaa");
-																	</script>';
-														}
-														/*echo	'<script type="text/javascript">
-																	alert("Hasta acá funciona");
-																</script>';*/
+													$var = $usuario->registrar();
+													if ($var) {
+														echo 'funciona';
+													} else {
+														echo 'no funciona';
 													}
+
+													Conexion::cerrar_conexion();
 												}
 											}
 										}
 									}
-										/*
-										Conexion::abrir_conexion();
-										$conexion = Conexion::obtener_conexion();
-										$usuario = new Usuario_Boundary($conexion, $documento, $nombres, $apellidos, $celular, $email, $direccion, $perfil);
-										$var = $usuario->registrar();
-										Conexion::cerrar_conexion();
-
-										/*echo	'<script type="text/javascript">'
-											, 		'location.reload();'
-											, 	'</script>'
-										;*/
-									}
-								
-							?>
+								}
+							}
+						?>
 					</div>
 				</div>
 			</div>
