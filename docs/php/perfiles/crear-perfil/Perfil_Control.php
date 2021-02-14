afefe8e37d6586c39668d6b6b7f3a25a2798a2f8
OscarGoFer <72538283+OscarGoFer@users.noreply.github.com>
2021-02-14 13:00:39 -0300

    Update Perfil_Control.php

--- docs/php/perfiles/crear-perfil/Perfil_Control.php
+++ docs/php/perfiles/crear-perfil/Perfil_Control.php
@@ -1,11 +1,7 @@
 <?php
-
 	class Perfil_Control{
-
 		public function __construct(){}
-
 		public function registrar($conexion, $nombre, $funciones){
-			
 			/* registro del nuevo perfil */
 			try {
 				$querySql="	INSERT INTO
@@ -21,7 +17,6 @@
 			} catch (Exception $e) {
 				print "ERROR: ".$e->getMessage();
 			}
-
 			/* query para obtener el id del perfil recién registrado */
 			try {
 				$querySql="	SELECT
@@ -30,11 +25,9 @@
 								perfil
 							WHERE
 								nombre='$nombre'";
-
 				$sentencia = $conexion->prepare($querySql);
 				$sentencia->execute();
 				$resultado = $sentencia->fetchAll();
-				
 				$id_perfil;
 				foreach ($resultado as $perfil) {
 					$id_perfil = $perfil['id'];
@@ -42,7 +35,6 @@
 			} catch (Exception $e) {
 				print "ERROR: ".$e->getMessage();
 			}
-			
 			/* registro de las funciones del perfil */
 			try {
 				for ($i=0; $i<count($funciones); $i++) {
@@ -55,18 +47,13 @@
 									'$id_perfil',
 									'$funciones[$i]'
 								)";
-
 					$sentencia = $conexion->prepare($querySql);
 					$sentencia->execute();
 				}
 			} catch (Exception $e) {
 				print "ERROR: ".$e->getMessage();
 			}
-			
 			return false;
-
 		}
-
 	}
-
 ?>
